626dc439983b26f5771703cff8f744f0b84261ac
VeuraGJ <yuanxiao_ru@163.com>
2019-07-23 23:54:15 +0800

    pop up confirm and implement go back

--- vue-practice-template-2019-7-18-13-28-25-557-master/src/components/home.vue
+++ vue-practice-template-2019-7-18-13-28-25-557-master/src/components/home.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <button>返回</button>
+    <button @click="goBack">返回</button>
   </div>
 </template>
 
 <script>
 export default {
-  
+  methods:{
+      goBack(){
+          let con = confirm("确认返回欢迎界面？")
+          if(con){
+              this.$router.go(-1);
+          }
+      }
+  }
 }
 </script>
 
